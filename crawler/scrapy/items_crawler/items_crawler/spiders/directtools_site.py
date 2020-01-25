92cd9fa0d0c2969efd7085729599c1720ada603d
apinto17 <apinto0704@gmail.com>
2020-01-25 11:10:27 -0800

    added directtools crawler

--- crawler/scrapy/items_crawler/items_crawler/spiders/directtools_site.py
+++ crawler/scrapy/items_crawler/items_crawler/spiders/directtools_site.py
@@ -3,7 +3,7 @@ from .site import Site
 import json
 
 
-class Bhid(Site):
+class Directtools(Site):
 
     # return terms of service else None
 
@@ -21,20 +21,14 @@ class Bhid(Site):
     # return a list of categories as browser objects
 
     def get_cats(self, response):
-        if(response.url == "https://www.bhid.com/"):
-            return response.css("ul.top-level-categories > li")
-        else:
-            return response.css("ul.categorycontent-container.list-unstyled > li")
+        return response.css("div.sub-category")
 
 
     # param bs object containing a category
     # return the link for that category
 
     def get_cat_link(self, cat, response):
-        if(response.url == "https://www.bhid.com/"):
-            return self.header + cat.css("a").attrib["href"]
-        else:
-            return self.header + cat.css("div.categorycontent-text > a").attrib["href"]
+        return cat.css("a").attrib["href"]
 
     # param browser object of the page
     # return the link to the show all page as a string if it exits
@@ -50,36 +44,26 @@ class Bhid(Site):
     def get_prod_pages(self, response):
     	return None
 
-    # return the link for the given prod page
-
-    def get_prod_page_link(self, page):
-    	return None
-
 
     # return the link of the next page button
 
     def get_next_page_link(self, response):
-        next_page = response.css("div.SearchResultPaging > a")[-1]
-        if("NEXT" in next_page.css("::text").get()):
-            return self.header + next_page.attrib["href"]
-        else:
-            return None
+        return response.css("a.page-links-next").attrib['href']
 
     # param browser object of the page
     # return a list of products as browser objects
 
     def get_prods(self, response):
-    	return response.css("div.SKULineWrapper")
+    	return response.css("div.category-product")
 
 
     def get_cat_string(self, response):
         res = ""
-        cats = response.css("div.SearchResultsLblInfo > span > a::text").getall()
-        cats.append(response.css("div.SearchResultsLblInfo > span > span::text").get())
+        cats = response.css("div.row.breadcrumbs > nav > ul > li > a::text").getall()[2:]
+        cats.append(response.css("span.current-item::text").get())
         for cat in cats:
-            cat = cat[:-1]
             if(len(cat) > 0):
-                res += "|" + cat
+                res += "|" + str(cat)
 
         return res
 
@@ -88,40 +72,49 @@ class Bhid(Site):
     # return item description as a string
 
     def get_item_desc(self, item):
-        return item.css("div.SKULineDescInfo > h2 > a::text").get()
+        return item.css("a > p::text").get()
 
     # param browser object of the item to be scraped
     # return item link as a string
 
     def get_item_link(self, item):
-    	return self.header + item.css("div.SKULineDescInfo > h2 > a").attrib['href']
+    	return item.css("a").attrib['href']
 
     # param browser object of the item to be scraped
     # return item image as a string
 
     def get_item_image(self, item):
-        return self.header + item.css("div.sku-image-enlarge > a").attrib['href']
+        return self.header + item.css("span.imgh > img").attrib["src"]
 
     # param browser object of the item to be scraped
     # return item price as a string
 
     def get_item_price(self, item):
-    	return item.css("span.PriceBreaks::text").get()
+    	return item.css("strong::text").get()
 
     # param browser object of the item to be scraped
     # return unit that the item is sold in as string ("box of 10")
 
     def get_item_unit(self, item):
-    	return item.css("span.SKULineUOM").attrib['data-uom']
+    	return None
 
     # param browser object of the item being scrapped
     # return all the specs of the item are returned as a string with the format {'key' : 'val'}
 
     def get_item_specs(self, item):
         res = {}
-        specs = item.css("div.item-specs > ul > li")
-        for spec in specs:
-            key = spec.css("label::text").get()[:-2].strip()
-            val = spec.css("div.itemSpecValues::text").get().strip()
-            res[key] = val
+        specs = []
+        specs = item.css("td.prodDetSpecTable > table > tr")
+        if(len(specs) > 0):
+            for spec in specs:
+                key = spec.css("td.prodDetSpecLeft::text").get()[:-1]
+                val = spec.css("td.prodDetSpecrT::text").get()[:-1]
+                res[key] = val
+        else:
+            specs = item.css("div.responsive-tabs > div > ul")[1].css("li::text").getall()
+            for spec in specs:
+                key = spec[:spec.index(":")]
+                val = spec[spec.index(":") + 2:]
+                res[key] = val          
+
         return json.dumps(res)
